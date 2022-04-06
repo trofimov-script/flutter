a38c989c13548b99bb5c94768c1dcdd2f188c49b
Dmitriy Trofimov <trofimov@spiks.ru>
2022-04-06 11:15:23 -0700

    feat: :sparkles: add script add bloc

---
+++ add_bloc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash    
+
+flutter pub add provider
+flutter pub add bloc
+flutter pub add flutter_bloc
+flutter pub add hydrated_bloc
+flutter pub add bloc_test
+
+flutter clean
+flutter pub get
+flutter pub upgrade
+flutter pub outdated
